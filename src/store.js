81ef324e93616bc35fbacb36fbef47467da9c3ad
stiles-micaiah <syluxprime40@gmail.com>
2019-07-06 00:00:18 -0600

    broken yeet

--- src/store.js
+++ src/store.js
@@ -1,16 +1,61 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
+const api = axios.create({
+  baseURL: "https://battlecardz.herokuapp.com/api/games"
+})
+
+
 export default new Vuex.Store({
   state: {
-
+    games: [],
+    activeGame: {}
   },
   mutations: {
+    setGames(state, data) {
+      state.games = data
+    },
+    setActiveGame(state, data) {
+      state.activeGame = data
+    }
 
   },
   actions: {
+    getGames({ commit, dispatch }) {
+      api.get('')
+        .then(res => {
+          commit('setGames', res.data)
+          console.log(res)
+        })
+        .catch(ree => {
+          console.error(ree)
+        })
+    },
+    getGame({ commit, dispatch }, id) {
+      api.get('' + id)
+        .then(res => {
+          commit('activeGame', res.data)
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    createGame({ commit, dispatch }, data) {
+      api.post('', data)
+        .then(res => {
+          commit('setActiveGame', res)
+          console.log('createGame output', res)
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    }
+
+
 
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home d-flex justify-content-center">
+    <div style="height: 300px; width: 300px;" class="card bg-dark text-white">
+      <img src="https://via.placeholder.com/300.png" class="card-img" alt="...">
+      <div class="card-img-overlay">
+        <h5 class="card-title">Card title</h5>
+
+
+        <div class="form-group">
+          <form @submit.prevent="startGame">
+            <input id="name" required="true" class="form-control" type="text" v-model="startGameInfo.playerName"
+              placeholder="Enter Your Name">
+            <select required='true' v-model="startGameInfo.set" class="custom-select">
+              <!-- <option selected="">Select Card Set</option> -->
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+              <option value="4">Four</option>
+            </select>
+            <button class="btn btn-sm btn-primary" type="submit">Play</button>
+          </form>
+        </div>
+
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        startGameInfo: {
+          playerName: "",
+          set: ""
+        }
+      }
+    },
+    mounted() {
+      this.$store.dispatch("getGames");
+    },
+    methods: {
+      startGame(data) {
+        this.$store.dispatch('createGame', this.startGameInfo)
+        console.log('startGame form output', this.startGameInfo)
+      }
+    }
   }
-}
 </script>
